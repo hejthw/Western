22a5fffb88c6c9bf04d3cfbb60bd39ae11a933df
hejthw <petrichenkomaksim86@gmail.com>
2026-01-21 04:34:49 +0500

    Added gravity

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,21 +1,49 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables: Speed
     public float walkSpeed;
     public float sprintSpeed;
+    [SerializeField] private float currentSpeed;
+    #endregion
     
+    #region Variables: Dependecies
     [SerializeField] private CharacterController characterController;
     [SerializeField] private CinemachineCamera cinemachineCamera;
-    [SerializeField] private float currentSpeed;
+    #endregion
+    
+    #region Variables: Gravity
+    private const float Gravity = -9.81f;
+    [SerializeField] private float _gravityMultiplier = 3.0f;
+    [SerializeField] private float _velocity;
+    private Vector3 gravityVector = new Vector3(0, 0, 0);
+    #endregion
     
     private Vector2 _move;
     
     private void Update()
     {
-        characterController.Move((GetForward() * _move.y + GetRight() * _move.x) * (Time.deltaTime * currentSpeed));
+        ApplyGravity();
+        characterController.Move((GetForward() * _move.y + GetRight() * _move.x + gravityVector) 
+                                 * (currentSpeed * Time.deltaTime));
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded && _velocity < 0.0f)
+        {
+            _velocity = -1;
+        }
+        else
+        {
+            _velocity += Gravity * _gravityMultiplier * Time.deltaTime;
+        }
+
+        gravityVector.y = _velocity;
     }
 
     private void Start()
