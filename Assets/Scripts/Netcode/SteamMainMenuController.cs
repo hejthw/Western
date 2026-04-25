03b1036913d7511548cab2ec9b2d64400065445e
hejthw <petrichenkomaksim86@gmail.com>
2026-04-25 10:41:54 +0500

    settings fix

--- Assets/Scripts/Netcode/SteamMainMenuController.cs
+++ Assets/Scripts/Netcode/SteamMainMenuController.cs
@@ -17,9 +17,11 @@ public class SteamMainMenuController : MonoBehaviour
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button quitButton;
     
-    [Header("Settings Buttons")]
+    [Header("Lobby Buttons")]
     [SerializeField] private Button backToMainButton;
 
+    [Header("Settings buttons")]
+    [SerializeField] private Button backToMainMenuButton;
     private SteamLobbyManager _lobbyManager;
     
     private void Awake()
@@ -71,11 +73,13 @@ public class SteamMainMenuController : MonoBehaviour
         quitButton.onClick.RemoveAllListeners();
         backToMainButton.onClick.RemoveAllListeners();
 
+
         createLobbyButton.onClick.AddListener(() => _lobbyManager.HostGame());
         joinLobbyButton.onClick.AddListener(() => _lobbyManager.OpenFriendsOverlay());
         settingsButton.onClick.AddListener(OpenSettings);
         quitButton.onClick.AddListener(Application.Quit);
         backToMainButton.onClick.AddListener(CloseSettings);
+        backToMainMenuButton.onClick.AddListener(CloseSettings);
     }
 
     private void WireLobbyEvents()
